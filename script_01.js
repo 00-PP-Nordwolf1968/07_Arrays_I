9f5ae40889ab2d938113a3aac6d4037a50c14ffe
Nordwolf1968 <fachinformatik@yahoo.com>
2023-07-19 10:25:05 +0200

    funktionalität mit array

--- script_01.js
+++ script_01.js
@@ -8,17 +8,15 @@
  */
 
 /*** 02. Theorie: Array */
-let arr;
-// arr = new Array();  //Konstruktor
-arr = [];              // Literal
-arr = [2,7,11,4,50];    // Daten im Array = Elements
-
-output(arr);
-output(arr.length);     // Anzahl der Elemente
-output(arr[0]);         // Index 0 = 1. Position im Array
-output(arr[4]);   // letzte Position / konkret
-output(arr[arr.length-1]);      // letzte Position / allgemein
-
+// let arr;
+// // arr = new Array();  //Konstruktor
+// arr = [];              // Literal
+// arr = [2,7,11,4,50];    // Daten im Array = Elements
+// output(arr);
+// output(arr.length);     // Anzahl der Elemente
+// output(arr[0]);         // Index 0 = 1. Position im Array
+// output(arr[4]);   // letzte Position / konkret
+// output(arr[arr.length-1]);      // letzte Position / allgemein
 /**
  *   PRAXIS
  * 
@@ -28,7 +26,6 @@ output(arr[arr.length-1]);      // letzte Position / allgemein
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
-
 // output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
 function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
@@ -43,6 +40,28 @@ function getSentence(word1,word2,word3,word4,word5,word6) {
 
     return str;
 }
+
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
 
 
 

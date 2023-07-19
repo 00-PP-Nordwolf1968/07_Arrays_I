e2f2d1531633491045f081d40e7a48d779597bc0
Nordwolf1968 <fachinformatik@yahoo.com>
2023-07-19 10:08:58 +0200

    elemente und positionen

--- script_01.js
+++ script_01.js
@@ -8,9 +8,16 @@
  */
 
 /*** 02. Theorie: Array */
+let arr;
+// arr = new Array();  //Konstruktor
+arr = [];              // Literal
+arr = [2,7,11,4,50];    // Daten im Array = Elements
 
-
-
+output(arr);
+output(arr.length);     // Anzahl der Elemente
+output(arr[0]);         // Index 0 = 1. Position im Array
+output(arr[4]);   // letzte Position / konkret
+output(arr[arr.length-1]);      // letzte Position / allgemein
 
 /**
  *   PRAXIS
@@ -22,13 +29,16 @@
 // --> "Ich bin Max Mütze."
 // output(getSentence("Ich","bin","Max","Mütze"));
 
-function getSentence(word1,word2,word3,word4) {
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) {
     const GAP = " ";
     const PUNCT = ".";
     let str =   word1 + GAP +
                 word2 + GAP +
                 word3 + GAP +
                 word4 + GAP +
+                word5 + GAP +
+                word6 +
                 PUNCT;
 
     return str;
